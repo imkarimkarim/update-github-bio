903dcecb1e98f1343040fd3c0b0b7166de46254d
imkarimkarim <imkarimkarim@pm.me>
2023-01-25 12:45:06 +0330

    revert: change to old message

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ async function init() {
     if (!total) return;
 
     const today = format(Date.now(), "yyyy-MM-dd");
-    const bioMessage = `the evil language(JS) developer, coded ${total} today(${today})`;
+    const bioMessage = `aka Alireza Madani, coded ${total} today(${today})`;
 
     await updateBio(bioMessage);
   } catch (e) {
